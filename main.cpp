ef8ce17f4081deba213285f64be32f2d3fad31bd
Habiba <habibabassem@aucegypt.edu>
2019-04-10 12:14:59 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  soukUp
+//
+//  Created by Habiba Bassem on 4/10/19.
+//  Copyright © 2019 Habiba Bassem. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
